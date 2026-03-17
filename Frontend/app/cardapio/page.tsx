0e52ddcd9c2d22422a2d73fbecc14db17b2e70bb
Antony Costa <antonycosta210@gmail.com>
2026-03-17 15:17:33 -0300

    Ajuste nas pastas

--- Frontend/app/cardapio/page.tsx
+++ Frontend/app/cardapio/page.tsx
@@ -1,7 +1,27 @@
-export default function CardapioPage() {
+async function getMenu() {
+  const response = await fetch("http://localhost:8000/menu",
+    {cache: "no-store",}
+  );
+
+  if (!response.ok) {
+    throw new Error("Erro ao buscar o cardápio");
+  }
+
+  return response.json();
+}
+export default async function CardapioPage() {
+  const itens = await getMenu();
+
   return (
     <main>
       <h1>Cardapio do dia</h1>
+      <ul>
+        {itens.map((item: any) => (
+          <li key={item.id}>
+            {item.name} - R$ {item.price}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
